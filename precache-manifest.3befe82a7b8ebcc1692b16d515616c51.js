d17a6640b5423a79550f1e79fb2999a912b2fdc4
rahulyapotla <prahulya@gmail.com>
2024-06-23 22:00:07 -0400

    Updates

---
+++ precache-manifest.3befe82a7b8ebcc1692b16d515616c51.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05905e87ef91b831905e6a77682a079c",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "66ae0886ac1216424b19",
+    "url": "/MyPortfolio/static/css/2.3f8589c5.chunk.css"
+  },
+  {
+    "revision": "66ae0886ac1216424b19",
+    "url": "/MyPortfolio/static/js/2.c41b5653.chunk.js"
+  },
+  {
+    "revision": "4c25d06d95384d1d9cff7a908ed8d725",
+    "url": "/MyPortfolio/static/js/2.c41b5653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ce1c5402f46afa0627",
+    "url": "/MyPortfolio/static/js/3.a65827c0.chunk.js"
+  },
+  {
+    "revision": "70830729e132df1ce846",
+    "url": "/MyPortfolio/static/js/main.1f67bb97.chunk.js"
+  },
+  {
+    "revision": "564210c4405ad3e45290",
+    "url": "/MyPortfolio/static/js/runtime-main.838f472d.js"
+  },
+  {
+    "revision": "6c65d5f16631fff0fac8a9688b8ff972",
+    "url": "/MyPortfolio/static/media/PH24My.6c65d5f1.jpeg"
+  },
+  {
+    "revision": "0822b25ff8719398a63c245344a2f40e",
+    "url": "/MyPortfolio/static/media/dummyImage.0822b25f.svg"
+  },
+  {
+    "revision": "db0721b19bf1b047866172744342786f",
+    "url": "/MyPortfolio/static/media/github_original.db0721b1.svg"
+  },
+  {
+    "revision": "1af820559282a0b98e0f8e3371b7db5e",
+    "url": "/MyPortfolio/static/media/hackathon.1af82055.jpeg"
+  },
+  {
+    "revision": "ab46037f2bb51cacbd0e7bea5a8cf3a6",
+    "url": "/MyPortfolio/static/media/linkedin_original.ab46037f.svg"
+  },
+  {
+    "revision": "9ceac4aae164b86f3b382079c0d78949",
+    "url": "/MyPortfolio/static/media/profile.9ceac4aa.jpeg"
+  },
+  {
+    "revision": "317711584c1ab9ea8e206c9d56b35941",
+    "url": "/MyPortfolio/static/media/resume.31771158.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MyPortfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MyPortfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MyPortfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MyPortfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ static/js/3.a65827c0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprpotla=this.webpackJsonprpotla||[]).push([[3],{65:function(t,n,e){"use strict";e.r(n);var a=e(0),o=e.n(a);n.default=function(){return o.a.createElement("div",null,"404 Page not found")}}}]);
+//# sourceMappingURL=3.a65827c0.chunk.js.map
